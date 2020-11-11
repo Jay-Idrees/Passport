e7eefa477ddea8e071a04dc614a9423b63893c09
Jay Idrees <idreesjay@gmail.com>
2020-11-10 22:07:10 -0500

    added git ignore to prevent node modules from loadig into the repository

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,6 +14,7 @@ module.exports = function(app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", function(req, res) {
+    // What is the structure here, are these two usernames and passwords? 
     db.User.create({
       email: req.body.email,
       password: req.body.password
